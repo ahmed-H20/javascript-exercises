4308a1c68c142596396f96ede2698a6e28e7e345
ahmed-H20 <ahmedheshamahah8@gmail.com>
2023-09-07 04:54:34 +0300

    Make 01 solution for function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(theArray, ...selector) {
+    let newArray = [];    
+    theArray.forEach((item) => {
+        if(!selector.includes(item)) {newArray.push(item)};        
+    });
+    return newArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
